cbce1896627b36cd530dd9b9f4a03a5fbb043076
Leif Denby <leif@denby.eu>
2025-09-12 21:50:51 +0200

    update main repo path

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ import (
     "strconv"
     "fmt"
 
-    "github.com/lheijst/rtldavis/protocol"
+    "github.com/leifdenby/rtldavis/protocol"
     "github.com/jpoirier/gortlsdr"
 )
 const maxTr = 8
